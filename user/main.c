cea91bdc06ee1ae8ef015b6a95cd5331f97b2a91
Andrey Gribanov <agry@mail.ru>
2019-11-24 16:03:32 +0700

    start

---
+++ user/main.c
@@ -0,0 +1,52 @@
+
+//Задача:"Прикрутить" к МК STM32F407VET6 WIFI модуль ESP8266 со стоковой прошивкой. 
+//удаленнно TELNET приложением передать данные в МК
+//Модуль подсоединить к домашней сети в режиме абонента сети ,протокол обмена TCP,режим сервера,
+//переадресация портов настраиваем в роутере, периодическую смену IP провайдером решаем с помощью DNS сервера
+//hldns.ru периодическими обращениями к нему МК,фактически dns нам дает возможность нам обращаться к МК по постонному
+// IP адресу
+// ESP8266 управляется AT-командами по UART,имеет на борту свой МК,который можно перепрошить под свои задачи
+// без freeRTOS, хотя сразу видно,что решение разделить на отдельные подзадачи
+
+#include "stm32f4xx.h"
+
+
+
+
+ void init_RCC()//настройка тактирования,пользуемся картинкой CUBE MX
+{
+RCC->CR|=RCC_CR_HSEON; //Запускаем генератор HSE,внешний кварц
+while (!(RCC->CR & RCC_CR_HSERDY)) {}; // Ждем готовности
+	
+FLASH->ACR |= FLASH_ACR_PRFTEN; // Enable Prefetch Buffer.
+FLASH->ACR &= ~FLASH_ACR_LATENCY; // Предочистка.
+FLASH->ACR |= FLASH_ACR_LATENCY_2WS; // Two wait states
+FLASH->ACR |=FLASH_ACR_ICEN | FLASH_ACR_DCEN;// Instruction cache enable, Data cache enable
+	
+RCC->CFGR &=~RCC_CFGR_SW; // Очистить биты SW0, SW1
+RCC->CFGR |= RCC_CFGR_PPRE1_2;	//100: AHB clock divided by 2 for APB1
+RCC->CFGR |= RCC_CFGR_SW_PLL; // Переходим на PLL SW1=1	---!!!
+
+ //Config PLL,see page 162	 
+RCC->PLLCFGR=(4<<24) | (0<<16) | (84<<6) | 4 | RCC_PLLCFGR_PLLSRC_HSE; //   настраевается при сброшеном бите 	PLLON  в CR		
+RCC->CR |=    RCC_CR_PLLON;
+while (!(RCC->CR & RCC_CR_PLLRDY)){}; //wait for PLL ready 	
+
+}//end init_RCC()
+//*****************************************************************************************************************
+
+
+
+
+int main(void)
+{
+	
+init_RCC();
+
+
+
+
+
+
+}
+	
